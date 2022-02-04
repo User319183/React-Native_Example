f9fa8071ac3de171e21eb2f60a849b09ebc30476
Sathvik <88944590+User319183@users.noreply.github.com>
2022-02-03 21:07:22 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import {Text, Button, View} from "react-native";
+
+
+export default class App extends React.Component{
+render(){
+  return(
+    <View>
+    <Text>A simple react-native example to get your code working!</Text>
+    <Button color="red" title="Test_Button"></Button>
+    </View>
+
+
+  )
+}
+}
